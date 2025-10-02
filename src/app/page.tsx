8661543f737bee3e6b304fc5f50f544504b1726d
SashaSurkin <sashasurkin3@gmail.com>
2025-10-02 16:44:46 -0400

    update with world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 font-sans bg-white text-black">
-      <h1 className="text-5xl mb-6">hi! i'm sasha.</h1>
+      <h1 className="text-5xl mb-6">hi! i'm sasha. hello</h1>
       <p className="text-lg text-center max-w-xl mb-10">
        很高興認識你。
       </p>
